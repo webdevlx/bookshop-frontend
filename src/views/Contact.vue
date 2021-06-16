bd61e6a578b803c801031ddf6753278aa9394a1f
Lazizbek <juniordev1005@gmail.com>
2021-06-16 23:01:20 +0500

    vue-router va vuexni o'rnatdik

---
+++ src/views/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Bu bo'g'lanish sahifasi</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod odio impedit, blanditiis velit nobis culpa dolorum cupiditate possimus sed facilis magnam sint ullam corrupti distinctio, nesciunt voluptas dolore. A aspernatur velit, voluptatibus nisi ex deleniti tenetur magnam ut! Dolorem et fugiat aliquid eligendi quaerat optio odit cumque voluptate. Possimus quia eveniet cumque, sit dolor fugiat adipisci voluptates quod assumenda ipsum, odit praesentium quos excepturi delectus inventore fugit molestiae magni aut laboriosam optio perspiciatis sint, quam porro? Suscipit sit, cumque corporis ducimus quos assumenda dolores debitis amet porro autem. Deserunt eaque distinctio unde, numquam veritatis deleniti. Necessitatibus sapiente quos tempora. Rerum perferendis laudantium odio quam! Aspernatur quibusdam ratione consequatur iusto, facere quisquam reprehenderit magnam pariatur inventore voluptatibus sapiente dolor enim a, at, quia tempore rem mollitia non? Nobis eos minus exercitationem odio harum, hic eveniet aut cumque quisquam veniam inventore quod, non quidem ipsam animi voluptas, aperiam possimus sapiente quos quibusdam eius? Aliquam ratione officiis nemo. Consequatur odit error accusantium nobis, blanditiis odio praesentium explicabo sequi quis laboriosam. Facilis incidunt illo commodi fugit dolores repellat vero atque voluptatum quos quo, totam in omnis, voluptatem dolorem inventore laboriosam odio ipsa reprehenderit necessitatibus possimus minus voluptatibus! Commodi quod ex quis magnam deserunt similique?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contact",
+};
+</script>
+
+<style scoped>
+</style>
